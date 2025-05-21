46ca76b381dfb697e2e4e180a95fbc945d32f92b
Josiann Zhou <jzhou@konradgroup.com>
2025-05-21 11:30:07 -0700

    Glart Demo UI fixes

--- src/app/components/GLARTDemo/GLARTDemo.js
+++ src/app/components/GLARTDemo/GLARTDemo.js
@@ -7,16 +7,18 @@ export default function GLARTDemo() {
                 <h2>Digital Art Glove</h2>
                 <h2 className={styles.subheading}> - Project Highlight</h2>
             </section>
-            <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/xtl84rtYhtc?si=-gA1hOxhWV72Iz8r"
-                title="YouTube video player"
-                data-frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                data-referrerpolicy="strict-origin-when-cross-origin"
-                data-allowfullscreen>
-            </iframe>
+            <div className={styles.player}>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/xtl84rtYhtc?si=-gA1hOxhWV72Iz8r"
+                    title="YouTube video player"
+                    data-frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    data-referrerpolicy="strict-origin-when-cross-origin"
+                    data-allowfullscreen>
+                </iframe>
+            </div>
         </div>
     );
 }
