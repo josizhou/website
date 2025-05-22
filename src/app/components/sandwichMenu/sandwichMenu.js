77474509538f22c147f8f4f8905683422dd9fe8a
Josiann Zhou <jzhou@konradgroup.com>
2025-05-22 10:39:47 -0700

    sandwich menu overlay

--- src/app/components/sandwichMenu/sandwichMenu.js
+++ src/app/components/sandwichMenu/sandwichMenu.js
@@ -7,13 +7,19 @@ export default function SandwichMenu ({setIsMenuOpen}) {
     const router = useRouter();
 
     return (
-        <div className={styles.main}>
-            <div className={styles.closeIcon} onClick={() => setIsMenuOpen(false)}>
-                <Image src={closeIcon} alt='Close Button' width={25} height={25} draggable={false}/>
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div className={styles.closeIcon} onClick={() => setIsMenuOpen(false)}>
+                    <Image src={closeIcon} alt='Close Button' width={25} height={25} draggable={false}/>
+                </div>
+                <section className={styles.links}>
+                    <h2 className={styles.link} onClick={() => {
+                        router.replace('/');
+                        setIsMenuOpen(false);
+                    }}>Home</h2>
+                </section>
             </div>
-            <section className={styles.links}>
-                <h2 className={styles.link} onClick={() => router.replace('/')}>Home</h2>
-            </section>
+            <div className={styles.overlay} onClick={() => setIsMenuOpen(false)}/>
         </div>
     )
 }
